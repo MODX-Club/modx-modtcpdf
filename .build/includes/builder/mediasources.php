ef88dbb05f0342a33c5c3aaea4b06bbc4783d361
proxyfabio <proxyfabio@gmail.com>
2016-05-10 12:51:29 +0300

    Fix mediasources bundle

--- .build/includes/builder/mediasources.php
+++ .build/includes/builder/mediasources.php
@@ -1,105 +1,33 @@
 <?php
 
-# $mediaSources = include $sources['data'].'transport.mediasources.php';
-# if (!is_array($mediaSources)){
-#   $modx->log(modX::LOG_LEVEL_ERROR,'Adding MediaSources failed.'); }
-# else{
-#   $vehicleParams = array(
-#     xPDOTransport::PRESERVE_KEYS => false,
-#     xPDOTransport::UPDATE_OBJECT => false,
-#     xPDOTransport::UNIQUE_KEY => 'name',
-#     # xPDOTransport::RELATED_OBJECTS => false,
-#     # xPDOTransport::RELATED_OBJECT_ATTRIBUTES => array (
-#     #   # 'Snippets' => array(
-#     #   #   xPDOTransport::PRESERVE_KEYS => false,
-#     #   #   xPDOTransport::UPDATE_OBJECT => true,
-#     #   #   xPDOTransport::UNIQUE_KEY => 'name',
-#     #   # ),
-#     #   # 'Chunks' => array(
-#     #   #   xPDOTransport::PRESERVE_KEYS => false,
-#     #   #   xPDOTransport::UPDATE_OBJECT => true,
-#     #   #   xPDOTransport::UNIQUE_KEY => 'name',
-#     #   # ),
-#     # ),
-#   );
-#
-#   # foreach($mediaSources as & $mediaSource){
-#   #   $vehicle = $builder->createVehicle($mediaSource, $vehicleParams);
-#   #   $builder->putVehicle($vehicle);
-#   # }
-#   # $modx->log(modX::LOG_LEVEL_INFO,'Packaged in '.count($mediaSources).' MediaSources.'); flush();
-# }
-# unset($mediaSources,$vehicle,$vehicleParams);
+ $mediaSources = include $sources['data'].'transport.mediasources.php';
+ if (!is_array($mediaSources)) {
+     $modx->log(modX::LOG_LEVEL_ERROR, 'Adding MediaSources failed.');
+ } else {
+     $vehicleParams = array(
+     xPDOTransport::PRESERVE_KEYS => false,
+     xPDOTransport::UPDATE_OBJECT => false,
+     xPDOTransport::UNIQUE_KEY => 'name',
+      xPDOTransport::RELATED_OBJECTS => false,
+      xPDOTransport::RELATED_OBJECT_ATTRIBUTES => array(
+         'Snippets' => array(
+           xPDOTransport::PRESERVE_KEYS => false,
+           xPDOTransport::UPDATE_OBJECT => true,
+           xPDOTransport::UNIQUE_KEY => 'name',
+         ),
+         'Chunks' => array(
+           xPDOTransport::PRESERVE_KEYS => false,
+           xPDOTransport::UPDATE_OBJECT => true,
+           xPDOTransport::UNIQUE_KEY => 'name',
+         ),
+      ),
+   );
 
-
-
-$vehicleParams = array(
-    xPDOTransport::PRESERVE_KEYS => false,
-    xPDOTransport::UPDATE_OBJECT => false,
-    xPDOTransport::UNIQUE_KEY => 'target',
-    xPDOTransport::RELATED_OBJECTS => true,
-    xPDOTransport::RELATED_OBJECT_ATTRIBUTES => array (
-          'Target' => array(
-            xPDOTransport::PRESERVE_KEYS => false,
-            xPDOTransport::UPDATE_OBJECT => false,
-            xPDOTransport::UNIQUE_KEY => 'name',
-          ),
-    ),
-);
-
-# foreach($mediaSources as & $mediaSource){
-#     $vehicle = $builder->createVehicle($mediaSource, $vehicleParams);
-#     $builder->putVehicle($vehicle);
-# }
-
-$mediaSourceAccess = $modx->newObject("sources.modAccessMediaSource", array(
-    "principal_class" => "modUserGroup",
-    "principal" => 1,
-    "authority" => 0,
-    "policy"    => 8,
-));
-
-
-$params = array(
-  "basePath" => array(
-    "name" => "basePath",
-    "desc" => "prop_file.basePath_desc",
-    "type" => "textfield",
-    "options" => Array(),
-    "value" => 'core/components/'.PKG_PATH.'/import/',
-    "lexicon" => "core:source",
-  ),
-  "baseUrl" => Array
-  (
-    "name" => "baseUrl",
-    "desc" => "prop_file.baseUrl_desc",
-    "type" => "textfield",
-    "options" => Array(),
-    "value" => 'core/components/'.PKG_PATH.'/import/',
-    "lexicon" => "core:source",
-  ),
-  "skipFiles" => Array
-  (
-    "name" => "skipFiles",
-    "desc" => "prop_file.skipFiles_desc",
-    "type" => "textfield",
-    "options" => Array(),
-    "value" => '.svn,.git,_notes,nbproject,.idea,.DS_Store,.gitignore',
-    "lexicon" => "core:source",
-  )
-);
-
-$mediaSource = $modx->newObject('sources.modMediaSource', array(
- 'name' => 'modImporter import files',
- 'class_key' => 'sources.modFileMediaSource',
- 'description'   => 'Файлы импорта',//PKG_NAME_LOWER.' Core Source',
- 'properties' => $params,
-));
-
-$mediaSourceAccess->addOne($mediaSource, "Target");
-
-
-$vehicle = $builder->createVehicle($mediaSourceAccess, $vehicleParams);
-$builder->putVehicle($vehicle);
-
-$modx->log(modX::LOG_LEVEL_INFO,'Packaged in '.count($mediaSourceAccess).' MediaSourceAccess.'); flush();
+     foreach ($mediaSources as &$mediaSource) {
+         $vehicle = $builder->createVehicle($mediaSource, $vehicleParams);
+         $builder->putVehicle($vehicle);
+     }
+     $modx->log(modX::LOG_LEVEL_INFO, 'Packaged in '.count($mediaSources).' MediaSources.');
+     flush();
+ }
+ unset($mediaSources, $vehicle, $vehicleParams);
